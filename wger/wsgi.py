9ebeacdb9f64219f9dca43eda35818d80e1d5cc4
Clementm916 <clementm916@gmail.com>
2017-01-23 16:43:36 +0300

    using whitenoise

--- wger/wsgi.py
+++ wger/wsgi.py
@@ -16,12 +16,20 @@ framework.
 import os
 from django.core.wsgi import get_wsgi_application
 
+
+from whitenoise.django import DjangoWhiteNoise
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
+
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
+
+
+
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
